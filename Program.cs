7ec8e104f5116bc288c6e527eeff14533aa0cb7d
Anna Beatriz <annab_olivecom>
2022-04-08 19:43:48 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Cria variaveis em ponto flutuante
+double cm, m ,km;
+Console.Write("Entre com a medida (em metros):");
+m = Convert.ToDouble (Console.ReadLine());
+
+//Calcula equivalência de metros para quilômetros e centímetros
+cm = m * 100;
+km = m / 1000;
+
+Console.WriteLine("\n ---Equivalência---");
+Console.WriteLine($"{cm}");
+Console.WriteLine($"{m}");
+Console.WriteLine($"{km}");
